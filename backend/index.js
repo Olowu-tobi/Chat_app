b8c0487fcd6034a7f9f445d82067a6efd25dee60
Tobi Olowu <tobiolowu2001@gmail.com>
2024-04-08 12:14:45 +0100

    deploy still in process

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-// const path = require("path");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const mongoose = require("mongoose");
@@ -8,8 +7,6 @@ const { app, server } = require("./socket/socket");
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
-
-// __dirname = path.resolve();
 
 const mongodbUrl = process.env.MONGODB_URL;
 app.use(cors());
@@ -20,24 +17,7 @@ app.use(
   })
 );
 app.use(route);
-// app.use(express.static(path.join(__dirname, "/frontend/my_chat/dist")));
-// app.get("*", (req, res) => {
-//   const indexPath = path.join(
-//     __dirname,
-//     "frontend",
-//     "my_chat",
-//     "dist",
-//     "index.html"
-//   );
-//   res.sendFile(indexPath, (err) => {
-//     if (err) {
-//       console.error(`Error sending file: ${err}`);
-//       res.status(500).send("Error sending file");
-//     } else {
-//       console.log(`Sent file: ${indexPath}`);
-//     }
-//   });
-// });
+
 mongoose
   .connect(mongodbUrl, {
     useNewUrlParser: true,
