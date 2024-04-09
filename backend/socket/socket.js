6288020e87ef8f21fb1b1a24287a08b6b2ced412
Tobi Olowu <tobiolowu2001@gmail.com>
2024-04-09 13:36:20 +0100

    updated socketio url

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -6,12 +6,7 @@ const { Server } = socketIo;
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: ["http://localhost:5173"],
-    methods: ["GET", "POST"],
-  },
-});
+const io = new Server(server);
 
 const getReceiverSocketId = (receiverId) => {
   return userSocketMap[receiverId];
